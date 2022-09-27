cc83e47f5c6b5214576f47b4808d01d8ec8e5744
danymtz <dany.mtz.ayala@gmail.com>
2022-09-27 08:58:27 -0500

    v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from './services/request/request.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'segunda';
+
+  name: string = '';
+  order: string = '';
+
+  constructor(public requestService: RequestService){}
+
+  ngOnInit(): void {
+    this.boton();
+  }
+
+  boton(){
+    let boton = document.getElementById('btn');
+    let busca: HTMLInputElement = <HTMLInputElement> document.getElementById('search');
+    
+
+    boton?.addEventListener("click",() => {
+      console.log(busca.value);
+      this.requestService.getPokemon(busca.value).subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          this.name = resp.name;
+          this.order = resp.order;
+        }
+      }
+      );
+    })
+  }
 }
